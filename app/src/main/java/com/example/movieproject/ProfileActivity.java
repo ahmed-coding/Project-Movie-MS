a8e5ec43edd44aae922be0d24f496dfddd5fe78b
Ahmed M Hamza <71948774+ahmed-coding@users.noreply.github.com>
2023-10-20 03:03:16 +0300

    Complete the ProfileActivity Back-end and End the project.

--- app/src/main/java/com/example/movieproject/ProfileActivity.java
+++ app/src/main/java/com/example/movieproject/ProfileActivity.java
@@ -3,12 +3,24 @@ package com.example.movieproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    ImageView btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        btnBack = (ImageView) findViewById(R.id.backImage);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
